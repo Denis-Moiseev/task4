1fde0a4fd6f6190c32ae4b75f3fca541e002a59c
Denis-Moiseev <mrtesla666@gmail.com>
2023-07-10 02:59:35 +0800

    add program

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿double firstNumber = double.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+double secondNumber = double.Parse(Console.ReadLine());
+Console.Write("Введите третье число: ");
+double thirdNumber = double.Parse(Console.ReadLine());
+if (firstNumber < secondNumber)
+{
+   if (secondNumber < thirdNumber)
+    {
+        Console.Write($"Число { thirdNumber } максимальное");
+    }
+    else
+        Console.Write($"Число { secondNumber } максимальное");
+}
+else
+    if (firstNumber < thirdNumber)
+    {
+        Console.Write($"Число { thirdNumber } максимальное");
+    }
+    else
+        Console.Write($"Число { firstNumber } максимальное");
